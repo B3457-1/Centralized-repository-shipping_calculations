7cb7f1bdc6721ba57853a15e667b2467763968db
B3457-1 <bravoechoalphatango@gmail.com>
2024-11-23 01:32:14 +0100

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+ # Here is a new update by B3457-1
+git config --global user.email bravoechoalphatango@gmail.com
+git config --global user.name B3457-1
